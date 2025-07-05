0871b559c9c72ed15b6c877a0da873d7cb182407
Mohanraj <mohanmoaj970@gamil.com>
2025-07-05 15:42:23 +0530

    add problems.java

--- javalearner/src/com/java/coder.java
+++ javalearner/src/com/java/coder.java
@@ -2,9 +2,17 @@ package com.java;
 
 public class coder{
     public static void main(String[] args) {
-        int a=23;
-        int b=25;
-        System.out.println(a+b);
 
+                int n = 5;      // Change this number as needed
+                int fact = 1;   // Factorial starts with 1
+
+                int i = 1;
+                while(i <= n) {
+                    fact = fact * i;
+                    i++;
+                }
+
+                System.out.println("Factorial of " + n + " is: " + fact);
+            }
     }
-}
+
